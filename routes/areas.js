93dfa2e3e5d5be99372522284295bc302af3c206
soleilyasmina <mounlasolomon@gmail.com>
2018-12-10 11:45:20 -0500

    added get route

---
+++ routes/areas.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { Area } = require('../models');
+
+const areaRouter = express.Router();
+
+areaRouter.get('/', async (req, res) => {
+  try {
+    const areas = await Area.findAll();
+    res.json(areas);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({
+      message: e.message
+    })
+  }
+});
+
+module.exports = {
+  areaRouter
+}
