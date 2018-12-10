6e379357d5a49035d4f0b913c014de5366a3e564
soleilyasmina <mounlasolomon@gmail.com>
2018-12-10 11:49:40 -0500

    created individual get route

--- routes/areas.js
+++ routes/areas.js
@@ -15,6 +15,21 @@ areaRouter.get('/', async (req, res) => {
   }
 });
 
+areaRouter.get('/:id', async (req, res) => {
+  try {
+    const area = await Area.findOne({
+      where: { id: req.params.id }
+    });
+    res.json(area);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({
+      message: e.message
+    });
+  }
+});
+
+
 module.exports = {
   areaRouter
 }
