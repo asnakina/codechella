75d1c6258d6ae98b59760d979573266b7a991969
soleilyasmina <mounlasolomon@gmail.com>
2018-12-11 14:26:45 -0500

    restructured architecture, added view components

---
+++ client/src/components/areacomponents/AreaView.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class AreaView extends Component {
+  render() {
+    return (
+      <div className="AreaView">
+        <h3>AreaView</h3>
+      </div>
+    )
+  }
+}
---
+++ client/src/components/artistcomponents/ArtistView.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class ArtistView extends Component {
+  render() {
+    return (
+      <h3>ArtistView</h3>
+    )
+  }
+}
