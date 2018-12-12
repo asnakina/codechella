36c932c37d6be14603b95bb0b615cd636eada780
asnakina <asnakina1@gmail.com>
2018-12-12 15:45:05 -0500

    worked on areaModal

--- client/src/components/area/AreaView.js
+++ client/src/components/area/AreaView.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import AreaList from './AreaList';
 import AreaModal from './AreaModal';
 // import serv from '../services/apiservices.js';
+// import { Modal, Button } from 'antd';
 
 export default class AreaView extends Component {
   constructor(props) {
      super(props);
      this.state = {
       list: true,
-      selectedArea: ''
+      selectedArea: '',
+      areaModalVisibility: false
     }
     this.handleChange = this.handleChange.bind(this);
+    this.setModalVisibility = this.setModalVisibility.bind(this);
   }
 
   handleChange(selectedArea) {
@@ -19,15 +22,24 @@ export default class AreaView extends Component {
     });
   }
 
+  setModalVisibility(value) {
+    this.setState({
+      areaModalVisibility: value,
+    })
+  }
+
   render() {
     console.log(this.props.areas);
+    let areaModalVisibility = this.state.areaModalVisibility
     return (
       <div>
       {this.state.selectedArea ?
         <AreaModal
-          id={this.state.selectedArea}
-          artists={this.props.artists.filter(artist => artist.area_id === this.state.selectedArea)}
-          vendors={this.props.vendors.filter(vendor => vendor.area_id === this.state.selectedArea)}
+           id={this.state.selectedArea}
+           artists={this.props.artists.filter(artist => artist.area_id === this.state.selectedArea)}
+           vendors={this.props.vendors.filter(vendor => vendor.area_id === this.state.selectedArea)}
+           setModalVisibility={this.setModalVisibility}
+           visible={areaModalVisibility}
         /> :
         <AreaList
            areas = {this.props.areas}
