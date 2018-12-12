ac5092ed8de82a521c9df07b041d733c1f150646
soleilyasmina <mounlasolomon@gmail.com>
2018-12-12 11:31:39 -0500

    final css fix for artists and vendors

--- client/src/components/vendor/Vendor.js
+++ client/src/components/vendor/Vendor.js
@@ -3,15 +3,10 @@ import './VendorList.css';
 
 export default function Vendor(props) {
   return (
-<div id="wrapper">
-<div className="Vendor">
-
-<div className="img_wrapper">
-<img alt={props.vendor.name} src={props.vendor.img_url}/>
-<span><input className="favebutton" type="button" value="Add to Favorites"/></span>
-</div>
+    <div className="Vendor">
+      <img alt={props.vendor.name} src={props.vendor.img_url}/>
       <h3>{props.vendor.name.toUpperCase()}</h3>
+      <span><input className="favebutton" type="button" value="Add to Favorites"/></span>
     </div>
-  </div>
   )
 }
