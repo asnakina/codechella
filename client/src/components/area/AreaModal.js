0269d3f6c12a2868a5aed2ca80152fa538a87ee1
asnakina <asnakina1@gmail.com>
2018-12-13 19:15:16 -0500

    areas

--- client/src/components/area/AreaModal.js
+++ client/src/components/area/AreaModal.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Artist from '../artist/Artist';
 import Vendor from '../vendor/Vendor';
+import './Area.css';
 
 export default class AreaModal extends React.Component {
   constructor(props) {
@@ -15,22 +16,38 @@ export default class AreaModal extends React.Component {
 render() {
   return (
     <div className="AreaModalParent">
-      <button onClick={this.handleClose} className="AreaExit">X</button>
         <div className="AreaModalChild">
+        <button onClick={this.handleClose} className="AreaExit">X</button>
           <h3>Artists</h3>
             <div className="AreaModalArtistVendor">
               <div className="AreaModalArtist">
                {this.props.artists.map((artist) => {
                  return (
-                  <Artist artist={artist} />
-                 )
+                   <Artist key={artist.id}
+                      artist={artist}
+                      userArtists={this.props.userArtists}
+                      favoriteArtist={this.props.favoriteArtist}
+                      unfavoriteArtist={this.props.unfavoriteArtist}
+                      isFavorite={this.props.userArtists ?
+                        this.props.userArtists.some(userArtist => artist.id === userArtist.id) :
+                      false}
+                    />
+                  )
                })}
              </div>
            <h3>Vendors</h3>
            <div className="AreaModalVendor">
                {this.props.vendors.map((vendor) => {
                  return (
-                   <Vendor vendor={vendor} />
+                   <Vendor key={vendor.id}
+                      vendor={vendor}
+                      userVendors={this.props.userVendors}
+                      favoriteVendor={this.props.favoriteVendor}
+                      unfavoriteVendor={this.props.unfavoriteVendor}
+                      isFavorite={this.props.userVendors ?
+                        this.props.userVendors.some(userVendor => vendor.id === userVendor.id) :
+                      false}
+                    />
                   )
                 })}
                </div>
